0a4417532b65acf23648d79c6ce72ab78d4ada42
Code Cat <codecat@grr.la>
2023-03-21 18:38:46 +0000

    init commit

---
+++ config_properties.py
@@ -0,0 +1,182 @@
+import configparser
+import io
+
+
+def parse_properties_file(file_path: str):
+    """
+    Parses a Java properties *like* file and returns a dictionary with the properties.
+    """
+    with open(file_path, "r") as f:
+        return properties_string_to_dict(f.read())
+
+
+def properties_string_to_dict(content: str):
+    # remove BOM if present
+    content = content.lstrip("\ufeff")
+
+    # split content into lines
+    lines = content.split("\n")
+
+    # parse properties
+    properties = {}
+    for line in lines:
+        line = line.strip()
+        if not line or line.startswith("#") or line.startswith("!"):
+            continue
+        key, value = line.split("=", 1)
+        key = key.strip()
+        value = value.strip()
+
+        # create nested dictionaries for keys with dots
+        key_parts = key.split(".")
+
+        current_dict = properties
+        for i, key_part in enumerate(key_parts):
+            if i == len(key_parts) - 1:
+                # last key, assign value
+                current_dict[key_part] = value
+            else:
+                current_dict.setdefault(key_part, {})
+            current_dict = current_dict[key_part]
+
+    return properties
+
+
+def merge_dicts(*dicts):
+    """
+    Merge multiple dictionaries recursively.
+    """
+    result = {}
+    for d in dicts:
+        for k, v in d.items():
+            if isinstance(v, dict):
+                result[k] = merge_dicts(result.get(k, {}), v)
+            else:
+                result[k] = v
+    return result
+
+
+def parse_ini_file(file_path):
+    with open(file_path) as fh:
+        return ini_string_to_dict(fh.read())
+
+
+def ini_string_to_dict(content):
+    config = configparser.ConfigParser()
+    config.read_string(content)
+
+    config_dict = {}
+    for section in config.sections():
+        config_dict[section] = dict(config.items(section))
+
+    return config_dict
+
+
+def dict_to_ini_file(config_dict, output_fh):
+    config = configparser.ConfigParser()
+    config.read_dict(config_dict)
+    config.write(output_fh)
+
+
+def dict_to_ini_string(config_dict):
+    out = io.StringIO()
+    dict_to_ini_file(config_dict, out)
+    return out.getvalue()
+
+
+def dict_to_properties_file(config_dict, output_fh):
+    with output_fh:
+        output_fh.write(dict_to_properties_string(config_dict))
+
+
+def dict_to_properties_string(properties_dict, prefix=""):
+    """Convert a nested dictionary to Java properties format."""
+    properties = ""
+    for key, value in sorted(properties_dict.items()):
+        if isinstance(value, dict):
+            # Recursively convert nested dictionaries
+            nested_prefix = f"{prefix}{key}."
+            properties += dict_to_properties_string(value, nested_prefix)
+        else:
+            # Flatten keys and append to properties string
+            flat_key = f"{prefix}{key}"
+            properties += f"{flat_key}={str(value)}\n"
+    return properties
+
+
+def default_config():
+    """
+    Held in a common/shared package.
+    :return: dict of config
+    """
+    return {
+        "prod": {
+            "postgres": {
+                "db_host": "host-prod",
+                "db_port": 200,
+            },
+        },
+        "qa": {
+            "postgres": {
+                "db_host": "host-qa",
+                "db_port": 200,
+            },
+        },
+        "local": {
+            "postgres": {
+                "db_host": "host-dev",
+                "db_port": 100,
+            },
+        },
+    }
+
+
+def app_config():
+    return {
+        "sub_dyn_inventory": {
+            "topic": "SOW/INVENTORY",
+            "timeout": 300,
+        },
+        "prometheus": {
+            "port": 5790,
+        },
+        "sub_dyn_alog_prices": {
+            "topic": "SOW/PRICES",
+            "timeout": 100,
+        },
+    }
+
+
+def get_config(config_name):
+    overrides = {}
+
+    configs = {
+        "prod": {
+            "sub_dyn_inventory": {
+                "timeout": 700,
+            }
+        },
+        "qa": {
+            "sub_dyn_inventory": {
+                "timeout": 1200,
+            }
+        },
+        "local": {
+            "sub_dyn_inventory": {
+                "timeout": 900,
+            }
+        },
+    }
+
+    if config_name not in configs:
+        config = parse_properties_file(config_name)
+        if "mkv" in config:
+            config = config.pop("mkv")
+
+        config_name = config["generic"]["profile_name"]
+
+        overrides = config
+
+    return merge_dicts(
+        default_config()[config_name], app_config(), configs[config_name], overrides
+    )
---
+++ test_config_properties.py
@@ -0,0 +1,175 @@
+import io
+
+import pytest
+import config_properties
+
+
+def test_empty_properties_str():
+    assert config_properties.properties_string_to_dict("") == {}
+
+
+def test_missing_properties_file():
+    with pytest.raises(FileNotFoundError, match="missing"):
+        config_properties.parse_properties_file("missing")
+
+
+def test_properties_skips_comments_and_blank_lines():
+    content = """# empty file
+
+     # whitespace leading comment
+# no content
+"""
+    assert config_properties.properties_string_to_dict(content) == {}
+
+
+def test_properties_creates_flat_dict():
+    # using comment a dubious whitespace
+    content = """# line 1
+name=bob     
+status = bewildered    
+"""
+    assert config_properties.properties_string_to_dict(content) == {
+        "name": "bob",
+        "status": "bewildered",
+    }
+
+
+def test_properties_creates_nested_dict():
+    # using comment a dubious whitespace
+    content = """# line 1
+agent.name=bob     
+agent.status = bewildered    
+"""
+    assert config_properties.properties_string_to_dict(content) == {
+        "agent": {"name": "bob", "status": "bewildered"}
+    }
+
+
+def test_properties_creates_deeper_nested_dict():
+    # using comment a dubious whitespace
+    content = """# line 1
+agent.name=bob     
+agent.status = bewildered    
+heroes.batman = winged
+heroes.superman  = strong
+weapons.weak.stick   = bamboo
+weapons.weak.water   = wet
+weapons.strong.iron   = sword
+homes.batman.house = mansion
+homes.batman.work = cave
+
+"""
+    assert config_properties.properties_string_to_dict(content) == {
+        "agent": {"name": "bob", "status": "bewildered"},
+        "heroes": {"batman": "winged", "superman": "strong"},
+        "homes": {"batman": {"house": "mansion", "work": "cave"}},
+        "weapons": {
+            "strong": {"iron": "sword"},
+            "weak": {"stick": "bamboo", "water": "wet"},
+        },
+    }
+
+
+def test_properties_read_from_file():
+    assert config_properties.parse_properties_file("example.jini") == {
+        "mkv": {
+            "dyn_sub_algo": {"topic": "BIG/DATA"},
+            "generic": {
+                "log": {
+                    "dir": "/tmp/logs",
+                    "file": "mylog.txt",
+                },
+                "profile_name": "dev",
+                "project_name": "Project1",
+            },
+        }
+    }
+
+
+def test_merge_simple_dicts():
+    a = dict(a=1, b=1, c=1)
+    b = dict(b=2, d=2)
+    c = dict(c=3, e=3)
+
+    assert config_properties.merge_dicts(a, b, c) == {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 2,
+        "e": 3,
+    }
+
+
+def test_merge_nested_dicts():
+    a = {"a": {"aa": 10, "ab": 20, "ac": {"aca": 100}}}
+    b = {"a": {"ad": 30, "ac": {"aca": 200}}, "b": "top"}
+    c = {"a": {"ac": {"acb": "fish"}}}
+    assert config_properties.merge_dicts(a, b, c) == {
+        "a": {"aa": 10, "ab": 20, "ac": {"aca": 200, "acb": "fish"}, "ad": 30},
+        "b": "top",
+    }
+
+
+def test_ini_missing_file():
+    with pytest.raises(FileNotFoundError, match="missing"):
+        config_properties.parse_ini_file("missing")
+
+
+def test_init_read_from_file():
+    assert config_properties.parse_ini_file("example.ini") == {
+        "dyn_sub_algo": {"timeout": "200", "topic": "BIG/DATA"},
+        "generic": {
+            "log.dir": "/tmp/logs",
+            "log.file": "mylog.txt",
+            "profile_name": "dev",
+            "project_name": "Project1",
+        },
+    }
+
+
+def test_ini_creates_nested_dict():
+    content = """[top]
+    leader = boss
+    salary = 100
+    
+    [bottom]
+    best = Malcom
+    salary = 12
+    # ignored
+    """
+    assert config_properties.ini_string_to_dict(content) == {
+        "bottom": {"best": "Malcom", "salary": "12"},
+        "top": {"leader": "boss", "salary": "100"},
+    }
+
+
+def test_can_write_dict_as_ini():
+    config_dict = {
+        "bottom": {"best": "Malcom", "salary": "12"},
+        "top": {"leader": "boss", "salary": "100"},
+    }
+
+    expected = """[bottom]
+best = Malcom
+salary = 12
+
+[top]
+leader = boss
+salary = 100
+
+"""
+    assert config_properties.dict_to_ini_string(config_dict) == expected
+
+
+def test_merged_config_dicts():
+    actual = config_properties.get_config("qa")
+    expected = {
+        "postgres": {"db_host": "host-qa", "db_port": 200},
+        "prometheus": {"port": 5790},
+        "sub_dyn_alog_prices": {"timeout": 100, "topic": "SOW/PRICES"},
+        "sub_dyn_inventory": {"timeout": 1200, "topic": "SOW/INVENTORY"},
+    }
+    assert actual == expected
+
+    print(config_properties.dict_to_ini_string(actual))
+    print(config_properties.dict_to_properties_string(actual, prefix='mkv.'))
